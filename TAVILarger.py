fb29b4594481f7bd55c36bf51e9d9b5a7597dbe3
etubman <euan.tubman@medtronic.com>
2025-07-18 10:39:51 +0100

    Update TAVILarger.py

--- TAVILarger.py
+++ TAVILarger.py
@@ -24,7 +24,7 @@ def main():
     # Create two columns: one for the logo, one for the title
     col1, col2 = st.columns([1, 5])
     with col1:
-        st.markdown("<img src='freeman_logo.png' width='60' style='display: block; margin: 0 auto;'>" , unsafe_allow_html=True)
+        st.image("freeman_logo.png", width=50)
     with col2:
         st.title("Freeman Hospital TAVI Length of Stay and Risk Calculator")
 
